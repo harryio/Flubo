2fa5fd4646e58790b7af5ce420c62048ca4bea72
Harry Singh <me@harryio.com>
2017-06-05 02:57:19 +0530

    Set repeating alarms for repeating reminders

--- app/src/main/java/com/harryio/flubo/receiver/NotificationPublisher.java
+++ app/src/main/java/com/harryio/flubo/receiver/NotificationPublisher.java
@@ -8,7 +8,10 @@ import android.support.v4.app.NotificationManagerCompat;
 
 import com.harryio.flubo.data.ReminderDAO;
 import com.harryio.flubo.model.Reminder;
+import com.harryio.flubo.model.RepeatInterval;
 import com.harryio.flubo.utils.NotificationFactory;
+
+import java.util.Calendar;
 
 public class NotificationPublisher extends BroadcastReceiver {
     private static final String EXTRA_REMINDER_ID = "com.harryio.flubo.EXTRA_REMINDER_ID";
@@ -29,6 +32,37 @@ public class NotificationPublisher extends BroadcastReceiver {
                     .getReminderNotification(context, reminder);
             NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
             notificationManager.notify((int) id, notification);
+
+            if (reminder.getRepeatInterval() != RepeatInterval.ONE_TIME) {
+                setNextAlarm(reminder);
+            }
+        }
+    }
+
+    private void setNextAlarm(Reminder reminder) {
+        RepeatInterval repeatInterval = reminder.getRepeatInterval();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(reminder.getRemindTime());
+
+        switch (repeatInterval) {
+            case DAILY:
+                calendar.add(Calendar.DATE, 1);
+                break;
+
+            case WEEKLY:
+                calendar.add(Calendar.WEEK_OF_YEAR, 1);
+                break;
+
+            case MONTHLY:
+                calendar.add(Calendar.MONTH, 1);
+                break;
+
+            case YEARLY:
+                calendar.add(Calendar.YEAR, 1);
+                break;
+
+            default:
+                throw new IllegalStateException("No interval defined for " + repeatInterval.name());
         }
     }
 }
